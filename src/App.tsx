76db5cc4cd23005a923b4f9bb9b5c3f033d2e2de
Meegu80 <aceman80@nate.com>
2026-04-28 23:44:35 +0900

    Ver3.0 - 컴포넌트 활용

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,67 @@
-function App() {
-    // 1. 데이터 배열(['hello', 'world'])을 순회하며 React 엘리먼트 배열을 생성합니다.
-    const texts = ['hello', 'world'].map((text, index) =>
-        // 2. <p> 태그를 생성합니다.
-        // 여기서 children은 <p>hello</p>와 같이 태그 사이에 들어갈 내용을 의미합니다.
-        <p key={index} children={text}/>)
+import P from "./P.tsx";
 
-    // 3. 최종적으로 <div> 태그의 자식 요소로 위에서 만든 texts 배열을 전달합니다.
-    return <div children={texts} />
+function App() {
+    const texts = ['hello', 'world'].map((text, index) =>
+        <P key={index} children={text} />
+    )
+
+// 렌더링 파트(JSX & TSX = 태그를 사용할 수 있게해주는 구간)===================================================================
+   return (
+        <div children={texts} />
+   )
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
